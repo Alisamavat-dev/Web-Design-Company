738400cb1676a3d74de5a1d9a7ad3c2013f32253
Alisamavat-dev <alisamavat.dev@gmail.com>
2025-04-26 23:14:31 +0330

    v2

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import Header from "../../components/Header/Header";
 import Baner from "../../components/Home/Baner/Baner";
 import Card from "../../components/Home/Card/Card";
+import SEO from "../../components/Home/SEO/SEO";
 const Home = () => {
   return (
     <>
       <div className="">
+        <SEO />
         <Header />
       </div>
       <div className="pt-15 lg:pt-8">
